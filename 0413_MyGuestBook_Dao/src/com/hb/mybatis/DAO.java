805191c92891b7021f4e686ce959a3d2b1df0f9d
1027 <1027@HB04-00>
2017-07-07 09:41:39 +0900

    두번째 커밋

--- 0413_MyGuestBook_Dao/src/com/hb/mybatis/DAO.java
+++ 0413_MyGuestBook_Dao/src/com/hb/mybatis/DAO.java
@@ -8,7 +8,7 @@ import org.apache.ibatis.session.SqlSession;
 public class DAO {
 	private static SqlSession ss;
 	
-	// 싱글턴 패턴 ; 프로그램이 끝날때까지 해당 객체는 하나만 가지고 사용
+	// 싱글턴 패턴 ; 프로그램이 끝날때까지 해당 999 객체는 하나만 가지고 사용
 	private synchronized static SqlSession getSql(){
 		if(ss == null){
 			ss = DBService.getFactory().openSession(false);
